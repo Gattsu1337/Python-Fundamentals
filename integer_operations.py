6a29ba669809f735cebb65263aaddef875667f9e
Dimitar Velikov <102250347+Gattsu1337@users.noreply.github.com>
2022-05-21 09:46:03 +0300

    Add files via upload

---
+++ integer_operations.py
@@ -0,0 +1,8 @@
+first = int(input())
+second = int(input())
+third = int(input())
+fourth = int(input())
+sum_of_numbers = first + second
+division = sum_of_numbers // third
+multiplication = division * fourth
+print(multiplication)
