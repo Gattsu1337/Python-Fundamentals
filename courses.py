ca9391fb3851bd1571ad1ddefad803132c327304
Dimitar Velikov <102250347+Gattsu1337@users.noreply.github.com>
2022-06-17 13:33:55 +0300

    Add files via upload

--- courses.py
+++ courses.py
@@ -1,6 +1,22 @@
-number = int(input())
-courses = []
-for n in range(number):
-    current_word = input()
-    courses.append(current_word)
-print(courses)
+courses_dict = {}
+command = input()
+
+while command != 'end':
+
+    info = command.split(' : ')
+    current_course = info[0]
+    current_student = info[1]
+
+    if current_course not in courses_dict:
+        courses_dict[current_course] = []
+
+    courses_dict[current_course].append(current_student)
+
+    command = input()
+
+for key, value in courses_dict.items():
+
+    print(f"{key}: {len(value)}")
+
+    for student in value:
+        print(f"-- {student}")
